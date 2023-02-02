1cce56e344ec0d5f50c915b55cdfbdb904624ae8
dmstj920 <96936437+dmstj920@users.noreply.github.com>
2023-02-02 17:29:56 +0900

    [PGS] 신고 결과 받기
    https://campus.programmers.co.kr/courses/16301/lessons/153414

---
+++ 신고결과받기.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+def solution(id_list, report, k):
+    
+    reportHash = defaultdict(set)
+    stopHash = defaultdict(int)
+    
+    for i in report:
+        reportString = i.split()
+        reportHash[reportString[0]].add(reportString[1])
+    
+    for key in reportHash.keys():
+        for j in reportHash.get(key):
+            stopHash[j] += 1
+        
+    answer = []
+    for name in id_list:
+        mail = 0
+        for user in reportHash[name]:
+            if stopHash[user] >= k:
+                mail += 1
+                
+        answer.append(mail)
+    
+    return answer
