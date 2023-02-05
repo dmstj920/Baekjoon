cee762cc56358253d64a465096b26e7933a7ff15
dmstj920 <96936437+dmstj920@users.noreply.github.com>
2023-02-05 16:45:45 +0900

    [PGS] 주차 요금 계산

--- 프로그래머스/주차요금계산.py
+++ 프로그래머스/주차요금계산.py
@@ -1,6 +1,45 @@
 import math
 
-def calculate(hour1, minute1, hour2, minute2):
+#강의 참조
+def calculate(time):
+    hour, minute = time.split(":")
+    total = int(hour) * 60 + int(minute)
+    
+    return total
+    
+    
+def solution(fees, records):
+    inTime = [0]*10000
+    isIn = [0]*10000
+    sumT = [0]*10000
+    
+    cars = []
+    for i in records:
+        a, b, c = i.split()
+        b = int(b)
+        cars.append(b)            
+            
+        if c == "IN":
+            inTime[b] = calculate(a)
+            isIn[b] = 1
+        else:
+            sumT[b] += calculate(a) - inTime[b]
+            isIn[b] = 0
+            
+    
+    cars = sorted(set(cars))
+    answer = []
+    for j in cars:
+        if isIn[j] == 1:
+            sumT[j] += calculate("23:59") - inTime[j]
+        
+        fee = fees[1]+ max(math.ceil((sumT[j]-fees[0])/fees[2]),0) * fees[3]
+        answer.append(fee) 
+    
+    return answer
+
+#내 코드
+"""def calculate(hour1, minute1, hour2, minute2):
     hour1, minute1, hour2, minute2 = int(hour1), int(minute1), int(hour2), int(minute2)
     minute = minute2 - minute1
     hour = hour2 - hour1
@@ -58,3 +97,4 @@ def solution(fees, records):
             
         
     return answer
+    """
